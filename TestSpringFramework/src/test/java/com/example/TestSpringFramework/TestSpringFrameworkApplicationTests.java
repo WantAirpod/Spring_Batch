83f961decbdbd8bfd0779ffa2f475122bde7dc91
DESKTOP-VUSLBIC\PC <cjy920409@gmail.com>
2021-09-16 11:01:58 +0900

    04장_HelloWrold예제

--- TestSpringFramework/src/test/java/com/example/TestSpringFramework/TestSpringFrameworkApplicationTests.java
+++ TestSpringFramework/src/test/java/com/example/TestSpringFramework/TestSpringFrameworkApplicationTests.java
@@ -1,10 +1,10 @@
-package com.HelloWorld.HelloWorld;
+package com.example.TestSpringFramework;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloWorldApplicationTests {
+class TestSpringFrameworkApplicationTests {
 
 	@Test
 	void contextLoads() {
