a7bd6a2f596f2ebe672357214c757e8acc0878ca
JINYOUNGCHOI <cjy9249@tmon.co.kr>
2021-09-19 22:15:11 +0900

    20210919_Reader

---
+++ TestSpringFramework/src/main/java/com/example/TestSpringFramework/P255_Custom/ItemWriter/ItemWriter.java
@@ -0,0 +1,9 @@
+package com.example.TestSpringFramework.P255_Custom.ItemWriter;
+
+import com.example.TestSpringFramework.P255_Custom.Model.Customer;
+import org.springframework.context.annotation.Bean;
+
+public class ItemWriter {
+
+
+}
---
+++ TestSpringFramework/src/main/java/com/example/TestSpringFramework/P255_Custom/Main/Main.java
@@ -0,0 +1,46 @@
+package com.example.TestSpringFramework.P255_Custom.Main;
+
+import com.example.TestSpringFramework.P199_JobExeplorer.DemoApplication;
+import com.example.TestSpringFramework.P199_JobExeplorer.ExploringTasklet;
+import com.example.TestSpringFramework.P255_Custom.Model.Customer;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class Main {
+
+    @Autowired
+    JobBuilderFactory jobBuilderFactory;
+    @Autowired
+    StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Step copFileStep(){
+        return this.stepBuilderFactory.get("copyFileStep")
+                .<Customer,Customer> chunk(10)
+         //      .reader(customerItemReader("asd.xtt"))
+        //        .writer(ItemWriter())
+                .build();
+    }
+    @Bean
+    public Job job(){
+        return this.jobBuilderFactory.get("job").start(copFileStep()).build();
+
+    }
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Main.class,args);
+    }
+}
